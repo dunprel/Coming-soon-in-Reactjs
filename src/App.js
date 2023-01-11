e7c6c7838adef4e2729fa188d89518c3a0a94434
PRECIOUS DUNKWU <100343765+dunprel@users.noreply.github.com>
2023-01-11 03:54:49 +0100

    First commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
 
-function App() {
+import FlipClockCountdown from '@leenguyen/react-flip-clock-countdown'
+import '@leenguyen/react-flip-clock-countdown/dist/index.css'
+
+import './styles.css'
+
+// import video
+import videoBg from '../src/assets/video.mp4'
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <section className='page'>
+      {/* overlay */}
+      <div className='overlay'></div>
+      <video src={videoBg} autoPlay loop muted />
+      <div className='page__content'>
+        <h1>Launching Soon</h1>
+        <h3>
+          Leave your email and we'll let you know once the site goes live.
+        </h3>
+        <FlipClockCountdown
+          className='flip-clock'
+          to={new Date().getTime() + 48 * 3600 * 1000 + 5000}
+        />
+        <button className='btn'>Notify me</button>
+      </div>
+    </section>
+  )
 }
 
-export default App;
+export default App
